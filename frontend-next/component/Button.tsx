3bb7fe0913f0ce8811a52ba67dccf3549d71db53
Eba Teshale <ebateshale40@gmail.com>
2026-06-25 18:58:42 +0300

    sommm

--- frontend-next/component/Button.tsx
+++ frontend-next/component/Button.tsx
@@ -9,11 +9,27 @@
 //     </div>
 //   );
 // }
+// import React from "react";
+
+// export default function Button(props) {
+//   return (
+//     <button className={`btn ${props.class}`} onClick={props.onClick}>
+//       {props.text}
+//     </button>
+//   );
+// }
+
 import React from "react";
 
-export default function Button(props) {
+type ButtonProps = {
+  text: string;
+  className?: string;
+  onClick?: () => void;
+};
+
+export default function Button(props: ButtonProps) {
   return (
-    <button className={`btn ${props.class}`} onClick={props.onClick}>
+    <button className={`btn ${props.className}`} onClick={props.onClick}>
       {props.text}
     </button>
   );
