14790a719e318134973664c3790553eaad0ae285
Eba Teshale <ebateshale40@gmail.com>
2026-06-25 18:36:48 +0300

    dskjf

---
+++ frontend-next/src/lib/lang.ts
@@ -0,0 +1,13 @@
+export const translations = {
+  en: {
+    title: "Hello World",
+    posts: "Posts",
+    toggle: "Change Language",
+  },
+
+  am: {
+    title: "ሰላም ዓለም",
+    posts: "ፖስቶ",
+    toggle: "ቋንቋ ቀይ",
+  },
+};
